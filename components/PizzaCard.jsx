bef0a5567977b60042229a4894959545713483fa
Rupantar Chowdhury <rupantarchowdhury99@gmail.com>
2023-02-13 17:56:09 +0530

    First COmmit

---
+++ components/PizzaCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from "../styles/PizzaCard.module.css"
+import Image from 'next/image'
+
+const PizzaCard = () => {
+  return (
+    <div className={styles.container}>
+      <Image src="/img/pizza.png" alt="" width="100" height="100" />
+
+      <h1 className={styles.title}>FIORI DI ZUCCA</h1>
+      <span className={styles.price}>$19.90</span>
+      <p className={styles.desc}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </p>
+
+    </div>
+  )
+}
+
+export default PizzaCard
